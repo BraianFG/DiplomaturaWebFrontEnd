34490ac6f2e763cf3cac1f1c3af7f4a4e7beb978
BraianFG <55467665+BraianFG@users.noreply.github.com>
2022-09-19 17:00:09 -0300

    Add files via upload

--- Dolar/assets/js/scripts.js
+++ Dolar/assets/js/scripts.js
@@ -1,79 +1,131 @@
-ejecutar = async () => {
+let id = parseInt(0);
+let iterar;
 
-    let id = parseInt(0);
-    let agencia;
-    let dolarSolidario = parseInt(0);
+const ejecutar = async () => {
     const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
     const respuesta = await fetch(url);
     const respuestaJSON = await respuesta.json();
+    generarCard(respuestaJSON);
+}
 
-    for (let iterar of respuestaJSON) {
-        document.querySelector("#ver").innerHTML += ` 
-                <div class="card card${id}">
-                <div class="card-title">
-                <p class="title">${iterar.casa.nombre}</p>
-                </div>
-                    <div class="card-content">
-                    <p>Venta : ${iterar.casa.venta}</p>
-                    <p>Compra:${iterar.casa.compra}</p>
-                    </div>
-                </div>
-                </div>`;
+    const variables = () =>{
+        id = id + 1;
 
-                
         agencia = iterar.casa.agencia;
+        venta = iterar.casa.venta;
+        compra = iterar.casa.compra;
+    }
 
+    const generarCard = (respuestaJSON) => {
+        for (iterar of respuestaJSON) {
+
+        variables();
+
+            document.querySelector("#ver").innerHTML += ` 
+                    <div class="card card${id}">
+                    <div class="card-title">
+                    <p class="title">${iterar.casa.nombre}</p>
+                    </div>
+                        <div class="card-content card-content">
+                        <p>Venta : ${venta}</p>
+                        <p>Compra:${compra}</p>
+                        </div>
+                    </div>
+                    </div>`;
+
+            validar();
+        }
+    }
+
+    const validar = () => {
         switch (agencia) {
             case "311":
-                document.querySelector(".card2").style.display = "none";
+                document.querySelector(".card3").style.display = "none";
+                //Dolar Soja
                 break;
 
             case "141":
-                document.querySelector(".card8").style.display = "none";
+                document.querySelector(".card9").style.display = "none";
+                //Argentina
                 break;
 
             case "302":
-                document.querySelector(".card7").style.display = "none";
+                document.querySelector(".card8").style.display = "none";
+                //Dolar
                 break;
 
             case "399":
-                document.querySelector(".card5").style.display = "none";
+                document.querySelector(".card6").style.display = "none";
+                //Bitcoin
+                break;
+
+            case "406":
+                document.querySelector(".card7").style.display = "none";
+                //Bitcoin
                 break;
         }
+        CalcularDolarSolidario();
+        agregar();
 
-        id = id + 1;
-        console.log(iterar.casa)
-        if(iterar.casa.agencia === "349"){
-            dolarSolidarioCotizacion = iterar.casa.venta ;
-            dolarSolidarioCotizacion = parseInt(dolarSolidarioCotizacion);
+        agregarDolarTurista();
+    }
 
-            dolarSolidarioRecargo =   65 /100;
-            dolarSolidarioRecargo2 = dolarSolidarioCotizacion *  dolarSolidarioRecargo;
-            dolar = dolarSolidarioCotizacion + Math.trunc(dolarSolidarioRecargo2);
+    const CalcularDolarSolidario = () => {
+        if (agencia === "349") {
+            let dolarSolidarioCotizacion = iterar.casa.venta;
+            dolarSolidarioCotizacion = parseFloat(dolarSolidarioCotizacion);
+
+            let dolarSolidarioRecargo = 65 / 100;
+            let dolarSolidarioRecargo2 = dolarSolidarioCotizacion * dolarSolidarioRecargo;
+            dolar = dolarSolidarioCotizacion + dolarSolidarioRecargo2;
+            dolar = parseFloat(dolar);
 
         }
     }
-    agregar();
 
-}
+    const agregar = () => {
+        class dolarSolidario {
+            constructor(nombre, venta, id) {
+                this.id = id
+                this.nombre = nombre
+                this.venta = venta
+            };
+        }
+        cotizacion = new dolarSolidario('Dolar Solidario', dolar.toFixed(2), '1');
+
+        document.querySelector(".card0").innerHTML = ` 
+        <div class="card-title">
+        <p class="title">${cotizacion.nombre}</p>
+        </div>
+            <div class="card-content">
+            <p>Cotización: ${cotizacion.venta}</p>
+            </div>
+        </div>`;
+    }
+
+    const agregarDolarTurista = () => {
+        if (agencia === "406") {
+            dolarTuristaCotizacion = iterar.casa.venta;
+
+            class dolarTurista {
+                constructor(nombre, venta, id) {
+                    this.id = id
+                    this.nombre = nombre
+                    this.venta = venta
+                };
+            }
+            cotizacionTurista = new dolarTurista('Dolar Turista', dolarTuristaCotizacion, '2');
+            document.querySelector(".card00").innerHTML = ` 
+            <div class="card-title">
+            <p class="title">${cotizacionTurista.nombre}</p>
+            </div>
+                <div class="card-content">
+                <p>Cotización: ${cotizacionTurista.venta}</p>
+                </div>
+            </div>`;
+        }
+
+    }
+
 ejecutar();
 
-agregar = () => {
-    class dolarSolidario {
-        constructor(nombre, venta, id) {
-            this.id = id
-            this.nombre = nombre
-            this.venta = venta
-        };
-    }
-    cotizacion = new dolarSolidario('Dolar Solidario', dolar,'1');      
-
-    document.querySelector(".card9").innerHTML = ` 
-    <div class="card-title">
-    <p class="title">${cotizacion.nombre}</p>
-    </div>
-        <div class="card-content">
-        <p>Cotización: ${cotizacion.venta}</p>
-        </div>
-    </div>`;
-}
